fe9b87d2bec79d16a98232e8b37d38cfcc418b00
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2020-05-03 09:52:59 +0200

    Created base ARM Keil project

---
+++ BlackPill_app/MDK-ARM/EMBSYSLAB/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+embsyslab\startup_stm32f103xb.o: startup_stm32f103xb.s
---
+++ BlackPill_app/MDK-ARM/RTE/_BlackPill_app/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BlackPill_app' 
+ * Target:  'BlackPill_app' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
