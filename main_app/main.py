5ca3f11e14fe455e1a60c6229785a95a392f4d15
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2020-05-12 21:50:41 +0200

    Uncommented port and baudrate selection

--- main_app/main.py
+++ main_app/main.py
@@ -9,13 +9,13 @@ from app_utils import valid_input, uC_transmit, uC_receive
 
 print('Welcome to my ECG application!')
 # Get list of available ports
-#comports_list = list(map(lambda p: p.device, serial.tools.list_ports.comports()))
-#print('Available COM ports: ' + str(comports_list))
-#com_port = valid_input('COM port = ', lambda p: p in comports_list)
+comports_list = list(map(lambda p: p.device, serial.tools.list_ports.comports()))
+print('Available COM ports: ' + str(comports_list))
+com_port = valid_input('COM port = ', lambda p: p in comports_list)
 
-#baud_rate = input('Baud rate (bps) = ')
+baud_rate = input('Baud rate (bps) = ')
 
-serial_p = serial.Serial(port="COM6", baudrate=115200,
+serial_p = serial.Serial(port=com_port, baudrate=baud_rate,
                   bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 # This function is called periodically from FuncAnimation
