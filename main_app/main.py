0107ff90069eb6600a91d97c6211358e1f8dfc19
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2020-05-26 09:57:13 +0200

    Final touches

--- main_app/main.py
+++ main_app/main.py
@@ -21,19 +21,24 @@ sg.theme('DarkAmber')  # Add a touch of color
 window = sg.Window('ECG Heart Monitor', connect_layout(comports_list),
                    size=(400, 250), element_justification='center')
 
-ani, ax, values = None, None, None
+ani, ax, values, x_cnt = None, None, None, 0
+num_of_samples = 0
 # action callbacks
 def SSR():
+    global num_of_samples
     X = values['srate']
+    num_of_samples = int(X)*60
     print(X)
+    print('number of samples = ', num_of_samples)
     cmd = 'SSR ' + X + '$'
     uC_transmit(serial_p, cmd)
 
 def C1MWD():
-    global ax, ani
+    global ax, ani, x_cnt
     cmd = 'C1MWD$'
     uC_transmit(serial_p, cmd)
-    fig = plt.figure(1)
+    x_cnt = 0
+    fig = plt.figure(1,figsize=(15, 7), dpi=80)
     fig.canvas.mpl_connect('close_event', TEARUP)
     ax = fig.add_subplot(1, 1, 1)
     xs = []
@@ -47,6 +52,8 @@ def RHBR():
     hbr = uC_receive(serial_p)
     if hbr == -2:
         hbr = '-'
+    while hbr == -1:
+        hbr = uC_receive(serial_p)
     window['HBR'].update(hbr)
 
 def TEARUP(evt):
@@ -72,7 +79,7 @@ def animate(i, xs, ys):
     if plt.fignum_exists(1) == False:
         return
     
-    global ax, ani
+    global ax, ani, x_cnt
     # Read heart pulse from uC
     beat = int(uC_receive(serial_p))
     print(beat)
@@ -83,19 +90,20 @@ def animate(i, xs, ys):
         beat = 2048
 
     # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%M:%S'))
+    xs.append(x_cnt)
     ys.append(beat)
 
+    x_cnt = x_cnt+1
     # Limit x and y lists to 20 items
     #xs = xs[-30:]
     #ys = ys[-30:]
 
     # Draw x and y lists
     ax.clear()
-    ax.plot(xs, ys, color='r')
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+    ax.plot(xs, ys, color='r', linewidth=0.5)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(num_of_samples*0.1))
     plt.gca().set_ylim([0, 4096])
-    plt.gca().set_xlim([0, 80])
+    plt.gca().set_xlim([0, num_of_samples])
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
