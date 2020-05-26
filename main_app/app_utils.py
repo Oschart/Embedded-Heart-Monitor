e80f4d59409c6f225eff6c7187f0f16bc84af2b7
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2020-05-26 02:01:22 +0200

    Two GUI panels are designed and linked

--- main_app/app_utils.py
+++ main_app/app_utils.py
@@ -1,3 +1,12 @@
+import serial
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.ticker as ticker
+import PySimpleGUI as sg
+
+
+
 def valid_input(prompt, is_valid):
     res = input(prompt)
     while not is_valid(res):
@@ -5,18 +14,48 @@ def valid_input(prompt, is_valid):
         res = input(prompt)
     return res
 
+
 def display_brief_guide():
     print('Supported Commands: ')
     print('SSR(R): -> Sets sampling rate to R')
     return
-    
+
+
 def uC_transmit(serial_p, cmd):
     # Send command string in byte mode
     cmd += '$'
     serial_p.write(cmd.encode())
     serial_p.flush()
 
+
 def uC_receive(serial_p):
     res = serial_p.readline().decode()[:-1].replace('\x00', '')
-    
+
     return -2 if res[0] == '!' else -1 if res[0:2] == 'OK' else res
+
+
+
+def connect_layout(comports_list):
+    return [[sg.Text('Connection Panel', font=('Verdana', 12))],
+            [sg.Text('----------------')],
+            [sg.Text('Select COM port: '), sg.Combo(comports_list, key='com', size=(10, 1))],
+            [sg.Text('Baud Rate: '), sg.Input(key='baudrate', size=(10, 1))],
+            [sg.Text('')],
+            [sg.Button('Start', font=('Verdana', 12))],
+            [sg.Text('')],
+            [sg.Button('Exit', font=('Verdana', 12))]]
+
+def control_layout():
+    return [[sg.Text('Welcome!', font=('Verdana', 12))],
+            [sg.Text('----------------')],
+            [sg.Text('Control Panel', font=('Verdana', 12))],
+            [sg.Text('----------------')],
+            [sg.Text('Sampling Rate ', font=('Verdana', 12)), sg.Input(key='srate', size=(10, 5), 
+            font=('Verdana', 12)), sg.Button(button_text='Set', font=('Verdana', 12), key='SSR')],
+            [sg.Text('')],
+            [sg.Button('Collect & Plot ECG for 1 Min', font=('Verdana', 12), key='C1MWD')],
+            [sg.Text('')],
+            [sg.Button('Report Heart Beat Rate', font=('Verdana', 12), key='RHBR')],
+            [sg.Text('')],
+            [sg.Button('Exit', font=('Verdana', 12))]
+            ]
--- main_app/main.py
+++ main_app/main.py
@@ -5,24 +5,59 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.ticker as ticker
 
-from app_utils import valid_input, uC_transmit, uC_receive
+import PySimpleGUI as sg
+
+from app_utils import *
 
 print('Welcome to my ECG application!')
 
 # Get list of available ports
-comports_list = list(map(lambda p: p.device, serial.tools.list_ports.comports()))
+comports_list = list(
+    map(lambda p: p.device, serial.tools.list_ports.comports()))
 
-print('Available COM ports: ' + str(comports_list))
-com_port = valid_input('COM port = ', lambda p: p in comports_list)
+sg.theme('DarkAmber')  # Add a touch of color
 
-baud_rate = input('Baud rate (bps) = ')
 
-serial_p = serial.Serial(port=com_port, baudrate=baud_rate,
-                  bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+window = sg.Window('ECG Heart Monitor', connect_layout(comports_list),
+                   size=(400, 250), element_justification='center')
+
+ani, ax = None, None
+# action callbacks
+def SSR():
+    X = values['srate']
+    cmd = 'SSR ' + X + '$'
+    uC_transmit(serial_p, cmd)
+
+def C1MWD():
+    global ax, ani
+    cmd = 'C1MWD$'
+    uC_transmit(serial_p, cmd)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    xs = []
+    ys = []
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=10)
+    plt.show()
+
+def RHBR():
+    cmd = 'RHBR$'
+    uC_transmit(serial_p, cmd)
+
+def TEARUP():
+    cmd = 'TEARUP$'
+    uC_transmit(serial_p, cmd)
+
+button_actions = {
+    'SSR': SSR,
+    'C1MWD': C1MWD,
+    'RHBR': RHBR,
+    'TEARUP': TEARUP
+}
 
 # This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
 
+    global ax, ani
     # Read heart pulse from uC
     beat = int(uC_receive(serial_p))
     print(beat)
@@ -44,29 +79,30 @@ def animate(i, xs, ys):
     ax.clear()
     ax.plot(xs, ys, color='r')
     ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-    plt.gca().set_ylim([0,4096])
-    plt.gca().set_xlim([0,80])
+    plt.gca().set_ylim([0, 4096])
+    plt.gca().set_xlim([0, 80])
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
     plt.title('Heart Activity')
-    #plt.grid()
-
+    # plt.grid()
 
 while True:
-    cmd = input('>> ').upper()
-    uC_transmit(serial_p, cmd)
-    if cmd == "C1MWD":
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-        xs = []
-        ys = []
-        ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=10)
-        plt.show()
-    elif cmd == "RHBR":
-        hr = uC_receive(serial_p)
-        print('Heart beat rate = ' + hr + ' bpm')
-    elif cmd == "EXIT":
+    event, values = window.read()
+    print(values)
+    if event in (None, 'Exit'):     # if user closes window or clicks exit
         break
+    if event == 'Start':  
+        com_port = values['com']
+        baud_rate = int(values['baudrate'])
+        # Open the serial port
+        serial_p = serial.Serial(port=com_port, baudrate=baud_rate,
+                                 bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+        # Initiate control panel layout
+        window.close()
+        window = sg.Window('ECG Heart Monitor', control_layout(),
+                           size=(500, 400), element_justification='center')
+    if event in button_actions:
+        button_actions[event]()
 
-
+window.close()
